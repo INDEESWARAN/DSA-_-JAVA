d042d6d8fa1b3e663c3af1f6953f59fd3b8c1db9
INDEESWARAN <indeeswaranc@gmail.com>
2024-09-03 15:55:05 +0530

    Create sumofdigits.java

---
+++ sumofdigits.java
@@ -0,0 +1,16 @@
+public class Sum {
+
+	public static void main(String[] args) {
+       int n=172;
+       int sum=0;
+       int rem;
+       while(n>0) {
+    	   rem=n%10;
+    	   sum=sum+rem;
+    	   n=n/10;
+       }
+       System.out.println(sum);
+
+	}
+
+}
