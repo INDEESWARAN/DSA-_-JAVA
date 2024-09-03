d0c508d8b8ae4970a065fa182f10b2cc9ccc4294
INDEESWARAN <indeeswaranc@gmail.com>
2024-09-03 16:01:21 +0530

    Create orderingnumbers.java

---
+++ orderingnumbers.java
@@ -0,0 +1,25 @@
+import java.lang.Math;
+
+public class ordered_element {
+	
+	class Main{
+	    public static void main(String[] args) {
+	       int n=1749;
+	       int temp=n;
+	       int count=0;
+	       while(temp>0){
+	           count++;
+	           temp=temp/10;
+	       }
+	       while(n>0){
+	           int rem=n%((int)Math.pow(10,count-1));
+	           int t=rem;
+	           n=n/(int)Math.pow(10,count-1);
+	           System.out.println(n);
+	           n=t;
+	           count--;
+	       }
+	       
+	    }
+	}
+}
